4b87e5543b3f5eb390a49072c492f1acaf7c1c55
Al-Mansori <elmnsrim@gmail.com>
2023-12-27 17:58:15 +0200

    upload up and running

--- src/main/java/com/FCAI/SE/OrderifyPro/controller/CustomerController.java
+++ src/main/java/com/FCAI/SE/OrderifyPro/controller/CustomerController.java
@@ -2,11 +2,16 @@ package com.FCAI.SE.OrderifyPro.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
-    // RESTful endpoints for customer management
+    @GetMapping("/habal")
+    public String habal() {
+
+        return "habal";
+    }
 }
