10b1b6a87cc61eea1df53ecb0fa53dcc87327177
Al-Mansori <elmnsrim@gmail.com>
2023-12-30 12:47:57 +0200

    solved bug

--- src/main/java/com/FCAI/SE/OrderifyPro/services/CompoundOrderServiceImp.java
+++ src/main/java/com/FCAI/SE/OrderifyPro/services/CompoundOrderServiceImp.java
@@ -4,9 +4,16 @@ import org.apache.commons.math3.stat.descriptive.summary.Product;
 import org.springframework.stereotype.Service;
 
 import com.FCAI.SE.OrderifyPro.model.Customer;
+import com.FCAI.SE.OrderifyPro.model.Order;
 
 @Service
 
 public class CompoundOrderServiceImp implements IOrderService {
+
+    @Override
+    public boolean addOrder(Order order) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addOrder'");
+    }
 
 }
